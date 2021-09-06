6b4bafe81dd03e387684511fc4d020263280a55f
Drake Champagne <trivorak@gmail.com>
2021-09-05 23:57:19 -0400

    Updated Color Pixel File. Working

--- research/drawpixel.py
+++ research/drawpixel.py
@@ -1,6 +1,22 @@
 import math
 from PIL import Image
 
+# Create blank variables
+aList = []
+aListInt = []
+aWorkingList = []
+
+# Defaults
+width = 16
+height = 9
+
+# Define Functions
+def getHeight(pixels,rWidth,rHeight):
+	return math.ceil(math.sqrt((pixels*rHeight/rWidth)))
+
+def getWidth(pixels,rWidth,rHeight):
+	return math.ceil(rWidth*getHeight(pixels,rWidth,rHeight)/rHeight)  
+
 # Input Hex from local text file
 with open("input.txt","r") as f:
 	a = f.readlines()
@@ -8,13 +24,8 @@ with open("input.txt","r") as f:
 # Convert input into a string
 a = str(a)
 
-#Remove the [' & '] from string
+#Remove the [' & '] from string - Windows endline
 a = a[2:(len(a)-2)]
-
-# Create blank variables
-aList = []
-aListInt = []
-aWorkingList = []
 
 # Split by every 2 characters and append to "blank list"
 for i in range(0,len(a),2):
@@ -28,15 +39,20 @@ for element in aList:
 for i in range(0,3-len(aListInt)%3):
 	aListInt.append(0)
 
-# Input Ratio 16:9 for example
-width = 16
-height = 9
 
 totalPixels = aListInt
 
-# Functions
-def getHeight(pixels,rWidth,rHeight):
-	return math.ceil(math.sqrt((pixels*rHeight/rWidth)))
+#DBG
+print(len(totalPixels))
 
-def getWidth(pixels,rWidth,rHeight):
-	return math.ceil(rWidth*getHeight(pixels,rWidth,rHeight)/rHeight)  
+# Create a new File to draw pixels
+im = Image.new("RGB",(getWidth(len(totalPixels)/3,width,height),getHeight(len(totalPixels)/3,width,height)),(0,0,0))
+pixels = im.load()
+index = 0
+
+# Drawing Loop
+for i in range(0,len(totalPixels),3):
+	pixels[index%im.width,math.floor(index/im.width)] = (totalPixels[i],totalPixels[i+1],totalPixels[i+2])
+	index = index + 1
+
+im.save("outputPixels.png")
