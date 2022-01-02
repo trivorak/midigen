a165e7970a33187701d8af0a92b8eaf713f7b098
Drake Champagne <trivorak@gmail.com>
2022-01-01 23:08:52 -0600

    Added Snap to scale using arrays Changed for loop to while True loop for Scale array generation.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Refocus of this project to use a Raspberry Pi Zero 2 W with a camera to take pic
 ### Roadmap 
 ---
  Generate an array based on notes in scale
-- [ ] Snap to Key
+- [x] Snap to Key
 - [ ] RPI camera module
 - [ ] Image Color Look up 
 - [ ] Ever changing image every step (take picture between notes)
@@ -19,7 +19,9 @@ Refocus of this project to use a Raspberry Pi Zero 2 W with a camera to take pic
 
 - [x] Randomly select root note
 - [x] Generate a scale array to use for note looking (snap to key/scale)
-  - [ ] Refactor for loop into a while true loop (cleaner)
+  - [x] Refactor for loop into a while true loop (cleaner)
+- [x] Snap note to Key.
+- [x] Setup a folder structure for cleaner function imports
 - [ ] Randomly select scale (From an Array of Scales)
 
 
@@ -28,7 +30,7 @@ Refocus of this project to use a Raspberry Pi Zero 2 W with a camera to take pic
 
 ---
 
-The planning process of project is underway so there is no path yet into what will be required to run this code. It will be based around Python3. All current requirements have been included in the requirements.txt file 
+The planning process of project is underway, so there is no path yet into what will be required to run this code. It will be based around Python3. All current requirements have been included in the requirements.txt file
 
 ```bash
 pip install -r requirements.txt
--- modules/scale.py
+++ modules/scale.py
@@ -6,29 +6,41 @@ b = []
 major = [0,2,4,5,7,9,11]
 
 # Main Functions
+
 # Generic
 # Return Random Number between Range
-
 def ranBetweenRange(min,max):
 	return random.random()*(max-min)+min
 
 
 # Return Random Musical Root Number
-# 0.5 - 12.49 to keep rounding even across all numbers
+# 0.5 - 11.49 to keep rounding even across all numbers
 def ranRootNote():
 	return round(ranBetweenRange(-0.5,11.49))
-	
-root = ranRootNote()
-root = root - 12
 
-for i in range (0, len(major)*12):
-	resultValue = ((root+major[i%len(major)])+(12*divmod(i,len(major))[0]))
-	if resultValue >= 128:
-		break
-	elif resultValue >= 0:
-		b.append(resultValue)
-	
-	
-# Test		
-print('Root = ' + str(root+12))
-print(b)
+
+# Return Array of Scale Notes based on:
+# Root = 0 - 11
+# scale = what's your scale offset see major above
+# scaleArray = Empty array
+def generateScaleArray(root,scale,scaleArray):
+	scaleArray.clear()
+	i = 0
+	while True:
+		resultValue = ((root+scale[i%len(scale)])+(12*divmod(i,len(scale))[0]))
+		if resultValue >= 128:
+			break
+		elif resultValue >= 0:
+			scaleArray.append(resultValue)
+		i += 1
+
+# Return note based on note array
+# Rounds down to nearest note
+def snapToScale(scaleArray,note):
+	for i in range(0, len(scaleArray)):
+		if scaleArray[i] > note:
+			return(scaleArray[i-1])
+			break
+		elif scaleArray[i] == note:
+			return(scaleArray[i])
+			break
