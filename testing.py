300e9f395bb87e5d6eac793472bca38b23df7394
Drake Champagne <trivorak@gmail.com>
2022-01-07 22:21:05 -0600

    Beginning to work out the Image processing section of this project

--- testing.py
+++ testing.py
@@ -1,8 +1,11 @@
 import modules.normal as normal
 import modules.scale as scale
+import modules.image as image
 
 # Define Blank Array/List
 a=[ ]
 
 # Debug / Test Functions
-print(scale.generateScaleArray(scale.randomRootNote(),scale.generateRandomScale(),a))
+
+# print(scale.generateScaleArray(scale.randomRootNote(),scale.generateRandomScale(),a))
+print(image.getColor(1,2))
