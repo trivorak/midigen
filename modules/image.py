b951cc46639f61abe71a39e81c1573e529629da5
Drake Champagne <trivorak@gmail.com>
2022-01-18 13:02:18 -0600

    Image Modules
    Working Out Image Processing Module script.

--- modules/image.py
+++ modules/image.py
@@ -4,13 +4,10 @@ from . import normal
 # TO-DO Use this file pull in image and parse out the color
 # Try to use PIL/Pillow to do all the image conversion
 
-im = Image.open('output.png');
-width, height = im.size;
-
-print("Height = " + str(height));
-print("Width = " + str(width))
-
 # Get Color Function
-def getColor(w,h):
-    return im.getpixel((w,h))
+def getColor(w,h,image):
+    return image.getpixel((w,h))
 
+def getImageSize(image):
+    width,height = im.size
+    return(width,height)
--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import modules.normal as normal
 import modules.scale as scale
 import modules.image as image
@@ -6,6 +7,8 @@ import modules.image as image
 a=[ ]
 
 # Debug / Test Functions
-
 # print(scale.generateScaleArray(scale.randomRootNote(),scale.generateRandomScale(),a))
-print(image.getColor(1,2))
+# print(image.getColor(1,2))
+
+im = Image.open('output.png')
+print(image.getColor(100,100,im))
