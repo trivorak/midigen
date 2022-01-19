a787573c0c531a319cb70306a39440be4c69a495
Drake Champagne <trivorak@gmail.com>
2022-01-19 13:32:11 -0600

    argument parsing
    added basic argument parsing to pass image file to process. If none is passed then it defaults to output.png
    
    example:
    python3 ./testing imagefile.png

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import sys
 import modules.normal as normal
 import modules.scale as scale
 import modules.image as img
@@ -7,11 +8,15 @@ import modules.image as img
 a=[ ]
 
 # Import Image
-im = Image.open('output.png')
+if (len(sys.argv) > 1 ): 
+  im = Image.open(sys.argv[1])
+else:
+  im = Image.open('output.png')
+
 # Debug print color at pixel 100x100
-# print(img.getColor(100,100,im))
+print(img.getColor(100,100,im))
 # Debug Return Array of image color normalized 0 - 1 see modules 
- a = normal.process(img.getImageBWArray(img.convertToMonochrome(im)))
+# a = normal.process(img.getImageBWArray(img.convertToMonochrome(im)))
 # Debug Print Normal List
-print(a)
+# print(a)
 
