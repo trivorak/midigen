1e49877aa080c6410f5a9fc018ded17ab19145ae
Drake Champagne <trivorak@gmail.com>
2022-03-08 12:22:27 -0600

    Changed SVG Stroke Width & Updated TestScript for timelaps

--- research/testscript.sh
+++ research/testscript.sh
@@ -8,16 +8,24 @@ function getTime() {
 # Get New Timestamp
 timestamp="$(getTime)"
 
-# Make backup copies
+# Clear last set of images
 rm input.png
+rm input.txt
 rm output.png
 rm output.mid
 
 # Start New Session
 raspistill -o input.png
+xxd -p input.png input.txt
+python3 ~/git/midigen/research/drawlines.py
+python3 ~/git/midigen/research/drawcolorlines.py
+python3 ~/git/midigen/research/drawpixel.py
+python3 ~/git/midigen/research/svgcolorlines.py
 convert input.png -monochrome -filter point -resize 1000x128 output.png
 python3 ~/git/imgnoise/trial.py
 
+# Copy files to folder for timelaps
+cp input.png timelaps/input/"input_"$timestamp".png"
 cp output.png timelaps/output/"output_"$timestamp".png"
 cp output.mid timelaps/midi/"midi_"$timestamp".mid"
 
